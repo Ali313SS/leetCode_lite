33bc491bac067e804c9a612a8d4291ed08924930
Paula Essam <paulaessam8@gmail.com>
2025-04-29 03:34:02 +0300

    [200~Get Standing in Contest

---
+++ source/repos/AJudge/AJudge.Application/DtO/AnnouncementsDTO/AnnouncementsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AJudge.Application.DtO.AnnouncementsDTO
+{
+    public class AnnouncementsDTO
+    {
+        public int AnnouncementId { get; set; }
+        public int ContestId { get; set; }
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
